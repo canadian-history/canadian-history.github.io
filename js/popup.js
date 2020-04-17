ba4f00f06dfd23d9d32ebbf1307aee73f4ed7f29
Monlih <MonliH@users.noreply.github.com>
2020-04-17 16:10:48 -0400

    Added rellax

--- js/popup.js
+++ js/popup.js
@@ -4,6 +4,23 @@ function closePopup(id) {
 }
 
 function openPopup(id) {
-    document.getElementById("overlay-"+id).style.opacity = "1";
     document.getElementById("overlay-"+id).style.zIndex = "100";
+    document.getElementById("overlay-"+id).style.opacity = "1";
 }
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    let isEscape = false;
+
+    if ("key" in evt) {
+        isEscape = (evt.key === "Escape" || evt.key === "Esc");
+    } else {
+        isEscape = (evt.keyCode === 27);
+    }
+
+    if (isEscape) {
+        for (const x of Array(4).keys()) {
+            closePopup(x+1);
+        }
+    }
+};
