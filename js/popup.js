e0b24f43757b54530a9ab661e6718a67680ac94d
Monlih <MonliH@users.noreply.github.com>
2020-04-18 10:19:19 -0400

    Fixed back button

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,34 @@
+function whichTransitionEvent() {
+    var t,
+        el = document.createElement("fakeelement");
+  
+    var transitions = {
+        "transition"      : "transitionend",
+        "OTransition"     : "oTransitionEnd",
+        "MozTransition"   : "transitionend",
+        "WebkitTransition": "webkitTransitionEnd"
+    }
+  
+    for (t in transitions){
+        if (el.style[t] !== undefined){
+            return transitions[t];
+        }
+    }
+}
+
 function closePopup(id) {
-    document.getElementById("overlay-"+id).style.opacity = "0";
-    document.getElementById("overlay-"+id).style.zIndex = "-1";
+    document.getElementById("overlay-"+id).style.width = "0vw";
+    let transition_event = whichTransitionEvent();
+    $(`#${"overlay-"+id}`).one(transition_event, 
+    function() {
+        document.getElementById("overlay-"+id).style.zIndex = "-3";
+        document.getElementById("overlay-"+id).style.opacity = "0";
+    });
 }
 
 function openPopup(id) {
-    document.getElementById("overlay-"+id).style.zIndex = "100";
+    document.getElementById("overlay-"+id).style.zIndex = "1000";
+    document.getElementById("overlay-"+id).style.width = "100vw";
     document.getElementById("overlay-"+id).style.opacity = "1";
 }
 
