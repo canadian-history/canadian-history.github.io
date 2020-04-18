18b71ce37165a1649c342a993bcad31292602fdd
Monlih <MonliH@users.noreply.github.com>
2020-04-18 14:59:44 -0400

    Added more fwancy effect

--- js/popup.js
+++ js/popup.js
@@ -1,35 +1,19 @@
-function whichTransitionEvent() {
-    var t,
-        el = document.createElement("fakeelement");
-  
-    var transitions = {
-        "transition"      : "transitionend",
-        "OTransition"     : "oTransitionEnd",
-        "MozTransition"   : "transitionend",
-        "WebkitTransition": "webkitTransitionEnd"
-    }
-  
-    for (t in transitions){
-        if (el.style[t] !== undefined){
-            return transitions[t];
-        }
-    }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function closePopup(id) {
+async function closePopup(id) {
     document.getElementById("overlay-"+id).style.width = "0vw";
-    let transition_event = whichTransitionEvent();
-    $(`#${"overlay-"+id}`).one(transition_event, 
-    function() {
-        document.getElementById("overlay-"+id).style.zIndex = "-3";
-        document.getElementById("overlay-"+id).style.opacity = "0";
-    });
+    document.getElementById("overlay-button-"+id).style.opacity = "0";
+    await sleep(700);
+    document.getElementById("overlay-"+id).style.zIndex = "-3";
 }
 
 function openPopup(id) {
     document.getElementById("overlay-"+id).style.zIndex = "1000";
     document.getElementById("overlay-"+id).style.width = "100vw";
     document.getElementById("overlay-"+id).style.opacity = "1";
+    document.getElementById("overlay-button-"+id).style.opacity = "1";
 }
 
 document.onkeydown = function(evt) {
